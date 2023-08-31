4983d0e3cbb90acab9e5661718b84c6e89e03130
BadPythonCoder <nuh@uh.uh>
2023-08-31 21:02:39 +0200

    Added inventory system, selling not included

---
+++ src/inventory.js
@@ -0,0 +1,38 @@
+const { Database } = require("./db.js");
+const fs = require("fs");
+
+const db = new Database("../db/inventories.json"); // i am on linux, comment this out and use the other line of code!
+//const db = Database("..\\db\\inventories.json");
+
+const items = JSON.parse(fs.readFileSync("../items.json"));
+//const items = JSON.parse(fs.readFileSync("..\\items.json"));
+
+class Inventory {
+	constructor(id) {
+		this.id = id;
+		if(db.data[id] == undefined) {
+			db.data[id] = { "money": 0, "items": [] };
+			db.sync();
+		}
+	}
+
+	getItems() {
+		return db.data[this.id].items;
+	}
+
+	getMoney() {
+		return db.data[this.id].money;
+	}
+
+	buy(itemID) { // buys a rod
+		if (db.data[this.id].money < items.rods[itemID].price) {
+			return "insufficient_funds";
+		}
+
+		db.data[this.id].items.push(itemID);
+		db.data[this.id].money -= items.rods[itemID].price;
+		db.sync()
+
+		return "bought";
+	}
+}
