08410a296fa42ec4076d5f51099231e55f1d8cce
TheRedBlueCube3 <theredbluecube2@gmail.com>
2023-08-31 22:43:39 +0300

    g

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const { DB } = require('textsyntaxdb');
 const {
 	Client,
 	Events,
@@ -8,7 +7,6 @@ const {
 } = require('discord.js');
 const { token } = require('../config.json');
 const { fish, rods } = require('../items.json');
-const db = new DB('data.tdb');
 const prefix = 'f!';
 const client = new Client({
 	intents: [
@@ -18,21 +16,22 @@ const client = new Client({
 		GatewayIntentBits.GuildMessages
 	]
 });
+
 client.once(Events.ClientReady, c => {
 	console.log(c.user.tag);
 	c.user.setActivity(`${prefix}help for cmds`, {
 		type: ActivityType.Playing
 	});
 });
-function setMoney(amount, id) {
-	db.write('money', id, amount);
-}
+
 client.on(Events.MessageCreate, async msg => {
 	try {
 		if (msg.content === `${prefix}fish`) {
 			let randomFish = fish[Math.floor(Math.random() * fish.length)];
 			msg.reply('Fishing... (this will depend on your rod)');
-			setTimeout(() => {}, 1000 /* placeholder value, needs inventory import*/);
+			setTimeout(() => {
+				msg.reply(`Congratulations, you found: ${randomFish}!`);
+			}, 1000 /* placeholder value, needs inventory import*/);
 		}
 	} catch (err) {
 		await msg.reply('An error occurred, error:', err);
