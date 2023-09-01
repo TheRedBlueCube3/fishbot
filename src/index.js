e70622207e150ceb0b16b1c02f08fe91c369776e
BadPythonCoder <nuh@uh.uh>
2023-09-01 21:59:35 +0200

    did some 'fixes', tho they may have created more errors lol

--- src/index.js
+++ src/index.js
@@ -9,8 +9,7 @@ const _ = require('lodash');
 const { token } = require('../config.json');
 const { fish, rods } = require('../items.json');
 const { Inventory } = require('./inventory');
-const { Database } = require('./db');
-let db = new Database('../db/inventories.json');
+const { Database } = require("./db");
 const prefix = 'f!';
 const client = new Client({
 	intents: [
@@ -20,7 +19,7 @@ const client = new Client({
 		GatewayIntentBits.GuildMessages
 	]
 });
-let inventories = {};
+const db = new Database('../db/inventories.json');
 client.once(Events.ClientReady, c => {
 	console.log(c.user.tag);
 	c.user.setActivity(`${prefix}help for cmds`, {
@@ -28,30 +27,27 @@ client.once(Events.ClientReady, c => {
 	});
 });
 client.on(Events.MessageCreate, async msg => {
+	/*
 	if (msg.content === `${prefix}createprofile`) {
 		if (msg.author.id in db.data)
 			return await msg.reply('**You already have a profile!**');
 		let inventory = new Inventory(msg.author.id);
-		db.data[msg.author.id] = inventory;
 		await msg.reply('Profile successfully created!');
-	}
+	} inventory.js already takes care of this!!!
+	*/
 	if (msg.content === `${prefix}fish`) {
-		if (!(msg.author.id in db.data))
-			return await msg.reply(
-				'**You do not have a profile!**\nTo create one, run f!createprofile.'
-			);
+		let inventory = Inventory(msg.author.id, db)
 		let randomFish = fish[Math.floor(Math.random() * fish.length)];
-		db._load();
 		let timeInBetween = _.random(
-			rods[db.data[msg.author.id].usingRod].timeStart,
-			rods[db.data[msg.author.id].usingRod].timeEnd
+			rods[inventory.getUsingRod()].timeStart,
+			rods[inventory.getUsingRod()].timeEnd
 		);
 		await msg.reply('Fishing... (this will depend on your rod)');
 		setTimeout(async () => {
 			let rn = random(0, 1);
 			if (rn == 1) {
 				await msg.reply(`Congratulations, you found: ${randomFish}!`);
-				db.data[msg.author.id].fish.push(randomFish);
+				inventory.pushFish(randomFish);
 			} else {
 				await msg.reply(`You **did not** catch a fish. :picardia_sad:`);
 			}
