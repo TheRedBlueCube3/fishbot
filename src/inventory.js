cebd53ca6d8609d52755e0547f92f2abcdf5b08d
TheRedBlueCube3 <theredbluecube2@gmail.com>
2023-09-03 16:52:32 +0300

    pr #3 wait

--- src/inventory.js
+++ src/inventory.js
@@ -1,13 +1,14 @@
-//const { Database } = require('./db.js');
+// const { Database } = require('./this.db.js');
 const fs = require('fs');
 
-//const db = new Database('../db/inventories.json'); // i am on linux, comment this out and use the other line of code!
+// const this.db = new Database('../this.db/inventories.json'); // i am on linux, comment this out and use the other line of code!
 
 const items = require('../items.json');
 
 class Inventory {
 	constructor(id, db) {
 		this.id = id;
+		this.db = db;
 		if (db.data[id] == undefined) {
 			db.data[id] = {
 				money: 150,
@@ -23,34 +24,34 @@ class Inventory {
 	}
 
 	getItems() {
-		return db.data[this.id].items;
+		return this.db.data[this.id].items;
 	}
 
 	getMoney() {
-		return db.data[this.id].money;
+		return this.db.data[this.id].money;
 	}
 
 	getUsingRod() {
-		return db.data[this.id].usingRod;
+		return this.db.data[this.id].usingRod;
 	}
 
 	addMoney(amount) {
-		db.data[this.id].money += amount;
-		db.sync();
+		this.db.data[this.id].money += amount;
+		this.db.sync();
 	}
 
 	removeMoney(amount) {
-		db.data[this.id].money -= amount;
-		db.sync();
+		this.db.data[this.id].money -= amount;
+		this.db.sync();
 	}
 
 	buy(itemID, amount) {
 		// buys a rod
-		if (db.data[this.id].money < items.rods[itemID].price) {
+		if (this.db.data[this.id].money < items.rods[itemID].price) {
 			return 'insufficient_funds';
 		}
 
-		db.data[this.id].items.rods.push(items.rods[itemID]);
+		this.db.data[this.id].items.rods.push(items.rods[itemID]);
 		this.removeMoney(items.rods[itemID].price);
 
 		return 'bought';
@@ -58,29 +59,32 @@ class Inventory {
 
 	sell(itemID, price, amount) {
 		// sells a fish
-		if (!(itemID in db.data[this.id].items)) {
+		if (!(itemID in this.db.data[this.id].items)) {
 			return 'item_not_present';
 		}
-
-		db.data[this.id].items.splice(db.data[this.id].items.indexOf(itemID));
-		db.data.market[this.id].push({ item: items.fish[itemID] });
-		this.addMoney(price);
+		for (let i = 0; i < amount; i++) {
+			this.db.data[this.id].items.splice(
+				this.db.data[this.id].items.indexOf(itemID)
+			);
+			this.db.data.market[this.id].push({ item: items.fish[itemID] });
+		}
 
 		return 'sold';
 	}
 
 	setRod(itemID) {
-		if (!(itemID in db.data[this.id].items)) return 'item_not_present';
-		if (!(itemID in db.data[this.id].items.rods)) return 'item_not_rod';
-		db.data[this.id].usedRod = items.rods[itemID];
-		db.sync();
+		if (!(itemID in this.db.data[this.id].items)) return 'item_not_present';
+		if (!(itemID in this.db.data[this.id].items.rods)) return 'item_not_rod';
+		this.db.data[this.id].usingRod = items.rods[itemID];
+		this.db.sync();
 
 		return 'set';
 	}
-
-	pushFish(fish) {
-		db.data[this.id].items.fish.push(fish);
-		db.sync();
+	pushFish(fishType) {
+		this.db.data[this.id].items.fish.push(
+			items.rods.findIndex(fish => fish.name == fishType)
+		);
+		this.db.sync();
 	}
 }
 
